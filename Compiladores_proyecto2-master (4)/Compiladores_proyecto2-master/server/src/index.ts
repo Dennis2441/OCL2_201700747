c6ef17a6b71fa559566e2fc42f7d7091925cd871
Dennis <dennis2441@gmail.com>
2022-11-03 21:53:31 -0400

    daksaklnsa

---
+++ Compiladores_proyecto2-master (4)/Compiladores_proyecto2-master/server/src/index.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import { Table } from './Simbols/Table';
+import { Break } from './Expresiones/Break';
+import { Continue } from './Expresiones/Continue';
+import { Exception } from './utils/Exception';
+
+const parser = require('./Grammar/Grammar.js');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+app.use(cors());
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.post('/analizar', (req, res) => {
+  const { entrada } = req.body;
+  if (!entrada) {
+    return res.redirect('/');
+  }
+  const tree = parser.parse(entrada);
+  const tabla = new Table(null);
+
+  
+  tree.instructions.map((m: any) => {
+    const res = m.execute(tabla, tree);
+    if (res instanceof Break) {
+      const error = new Exception('Semantico',
+        `Sentencia break fuera de un ciclo`,
+        res.line, res.column);
+      tree.excepciones.push(error);
+      tree.console.push(error.toString());
+    } else if (res instanceof Continue) {
+      const error = new Exception('Semantico',
+        `Sentencia continue fuera de un ciclo`,
+        res.line, res.column);
+      tree.excepciones.push(error);
+      tree.console.push(error.toString());
+    }
+  });
+  
+  let obj = {
+    consola: tree.console,
+    errores: tree.excepciones
+  };
+  res.setHeader('Content-Type', 'application/json');
+  res.json(obj);
+});
+
+app.listen(port, err => {
+  return console.log(`server is listening on ${port}`);
+});
